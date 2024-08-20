292c8c562a3f7cf2df429e0f840ad4fb9185ad96
Rangerbearr <eli.orille@yahoo.com>
2024-08-20 13:19:13 -0500

    project set up completly with webpack stuff set up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import "./styles.css";
-
+import bgimage from "./pancake background image.jpg";
 console.log('hi');
-const image = document.getElementById('image');
-image
+
+const content = document.getElementById('content');
+// const image = document.getElementById('image');
+const image = document.createElement("img");
+image.id = 'image';
+image.src = bgimage;
+content.appendChild(image);
+// document.body.appendChild(image);
+
